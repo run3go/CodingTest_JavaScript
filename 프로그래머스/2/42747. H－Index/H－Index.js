f4e11e2e1029e4f7e0097ca2e52c7de0c87c8622
Jeongsu Park <68907573+run3go@users.noreply.github.com>
2025-04-20 18:49:20 +0900

    [level 2] Title: H-Index, Time: 11.89 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,8 +1,12 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a);
-    
-    for(let h = citations[0]; h >= 0; h--) {
-        const quoteCnt = citations.filter((cit) => cit >= h).length;
-        if(quoteCnt >= h) return h;
+    citations.sort((a,b) => b-a);
+    for(let i = citations[0]; i >= 0; i--) {
+        let temp = 0;
+        for(let j = 0; j < citations.length; j++) {
+            if(citations[j] >= i) {
+                temp++;
+            }
+        }
+        if(temp >= i) return i;
     }
 }
