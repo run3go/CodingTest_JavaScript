d0fac6703b8818a9427822a2ed32c089cfb0885d
Jeongsu Park <68907573+run3go@users.noreply.github.com>
2025-08-08 17:10:40 +0900

    [level 2] Title: 문자열 압축, Time: 5.42 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,23 @@
+function solution(s) {
+    let result = 1001;
+    for(let i = 1; i <= s.length; i++) {
+        let answer = "";
+        let target = s.slice(0, i);
+        let cnt = 1;
+        let idx = i;
+        while(idx < s.length) {
+            const current = s.slice(idx, idx + i);
+            if(target !== current) {
+                answer += `${cnt === 1 ? "" : cnt}${target}`;
+                target = current;
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+            idx += i;
+        }
+        answer += `${cnt === 1 ? "" : cnt}${target}`;
+        result = Math.min(result, answer.length);
+    }
+    return result;
+}
